dd9f7b0eb99c1161baefd435bacac962831e95f1
Bronson Chapman <bronson@fpcc.ca>
2022-06-11 10:28:20 -0700

    Modify app.js and app.test.js to create a functioning sample unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Platforms link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Platforms/i);
   expect(linkElement).toBeInTheDocument();
 });
