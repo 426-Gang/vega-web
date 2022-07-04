138fb6870a0ab78a4e5b66add7feb3fc1aadf88b
Owen Jaques <owenjaques2000@gmail.com>
2022-07-03 18:47:38 -0700

    added thanks for registering section

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -2,31 +2,40 @@ import {useState, useContext} from 'react';
 import UserRegistrationPageLayout from '../templates/UserRegistrationPageLayout.js';
 import RegisterUser from '../UI/organisms/RegisterUser.js';
 import {register} from '../../service/auth/RegisterManager.js';
+import {Row, Col} from 'react-bootstrap';
 
 import {UserContext} from '../../auth/UserProvider.js';
-import  { Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Register = (props) => {
 	
-	const { context } = props; 
-	const {user, setUserInfo,logout} = useContext(UserContext);
-	const [auth, setAuth] = useState(false);
+	const {user} = useContext(UserContext);
+	const [regs, setRegs] = useState(false);
 	console.log("Userinfo", user);
 	function onSubmit(userInfo){
 		register(userInfo)
 			.then(res => {
 				console.log("Response", res);
+				setRegs(true);
 			})
 	}
 
-		if(!auth){
+		if(!regs){
 			return (
 				<UserRegistrationPageLayout>
 					<RegisterUser onSubmit={onSubmit}/>
 				</UserRegistrationPageLayout>
 			);
 		} else {
-			return <Redirect to='/' />;
+			return (
+				<UserRegistrationPageLayout>
+					<Row>
+						<Col>
+							Thanks for registering, please go ahead and <a href='/login'>login here</a>.
+						</Col>
+					</Row>
+				</UserRegistrationPageLayout>
+			);
 		}
 }
 
